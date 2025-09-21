7028f440987d8c13f117e6b8e1e940f55704c88a
Vitkovskiy <vitkovskiy1912@mail.ru>
2025-09-21 11:57:47 +0300

    HW-3.1

---
+++ src/BonusMilesService.java
@@ -0,0 +1,8 @@
+public class BonusMilesService {
+    public int calculate (int a){
+        int bonus = 20;
+        int miles;
+        miles = a / bonus;
+        return miles;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        //Задание 1
-        System.out.println("Задание 1");
-        int sum = 20000;
-        int bonus = 20;
-
-        int milly = sum / bonus;
-        System.out.println("За эту покупку получено " + milly + " бонусных миль\n");
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price); // должно получиться 500
+        System.out.println(miles);
     }
 }
